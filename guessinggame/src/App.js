4a77349e3b3bbbed444eb1b9c40889679154be5a
Lesly Perez <leslyperez@Administrators-MacBook-Pro-3.local>
2017-10-18 06:35:01 -0400

    final guessing game

--- guessinggame/src/App.js
+++ guessinggame/src/App.js
@@ -14,6 +14,7 @@
 
 import React, { Component } from 'react';
 import UserStats from './UserStats';
+// import GameContainer from './GameContainer';
 import InitState from './InitState';
 import './App.css';
 
@@ -28,38 +29,57 @@ class App extends Component {
   }
 
   // function that assigns gametype,
-  // and max number for math random calculator
+  // and max number for math random method
   changeGameType(type, max){
     let stateCopy = Object.assign({}, this.state);
-    let randomNumber =  Math.floor(Math.random() * (max + 1)); 
+    let randomNumber = Math.floor(Math.random() * max) + 1;
     const gameType = this.state.gameType;
     const targetNumber = this.state.targetNumber;
     const maxNum = this.state.maxNum;
     stateCopy.targetNumber = randomNumber;
     stateCopy.gameType = type;
     stateCopy.maxNum = max;
-    console.log("rando: ",randomNumber); 
     this.setState(
       stateCopy
     );
   }
 
+  // function that checks if input matches target number,
+  // and tracks high score
   handleSubmit(event){
     // COPY OF STATE - STATECOPY
     let stateCopy = Object.assign({}, this.state);
     let currentInput = stateCopy.currentInput;
-    let guess = currentInput;
     let targetNum = stateCopy.targetNumber;
-    let standardHigh = stateCopy.standardHigh;
-    let expertHigh = stateCopy.expertHigh;
+    let highScore = stateCopy.highScore;
+    // update number of guesses each guess
     stateCopy.guessCount++;
 
-    // if user guesses target num congrat. them
-    if(guess == targetNum) {
-      alert("WINNER"); 
-    } else if(guess <= targetNum){
+    console.log("tar: ", targetNum);
+
+    // if user guesses target number congratulate them
+    if(currentInput == targetNum) {
+      alert("WINNER");
+
+      // if hs is not set, then set it
+      if(highScore == 0) {
+        highScore = stateCopy.guessCount; 
+      } else if(highScore > stateCopy.guessCount){ // if hs is greater than current # of guesses, update it
+        highScore = stateCopy.guessCount; 
+        alert("You beat your high score!");
+      } else if(highScore < stateCopy.guessCount){ // if hs is less than current # of guesses, keep old hs
+        highScore = highScore; 
+      } 
+
+      stateCopy.maxNum = 0;
+      stateCopy.targetNumber = 0;
+      stateCopy.guessCount = 0;
+      stateCopy.guessType = 0;
+      stateCopy.highScore = highScore;
+
+    } else if(currentInput <= targetNum){
       alert("Too low, guess again"); 
-    } else if(guess >= targetNum){
+    } else if(currentInput >= targetNum){
       alert("Too high, guess again"); 
     } 
 
@@ -69,26 +89,24 @@ class App extends Component {
     this.setState(stateCopy);
   }
 
+  // when user is typing comment,
+  // display it in input box 
+  // and set it = to currentComment
   updateCurrentInput(event){
-   const userGuess = event.target.value;
-
+   let userGuess = event.target.value;
    this.setState({
      currentInput: userGuess,
    });
   }
 
   resetGame(){
-    console.log("reset");
     this.setState({
-      currentInput: '',
       gameType: "",
+      currentInput: '',
       maxNum: 0,
       targetNumber: 0,
       guessCount: 0,
-      currentMessage: '',
-      gamesPlayed: 0,
-      standardHigh: 0,
-      expertHigh: 0,
+      highScore: 0,
     })
   }
 
@@ -98,21 +116,21 @@ class App extends Component {
         <div className="startpage">
           <h1>Start Game</h1>
           <section className="buttons">
-            <button onClick={()=>{this.changeGameType("standard", 10)}}>Standard</button>
-            <button onClick={()=>{this.changeGameType("expert", 100)}}>Expert</button>
+            <button className="gameTypeBtn" onClick={()=>{this.changeGameType("Standard", 10)}}>Standard</button>
+            <button className="gameTypeBtn" onClick={()=>{this.changeGameType("Expert", 100)}}>Expert</button>
           </section>
         </div>
-       
         <div className="gamecontainer">
-            <section className="inputcontainer">
-              <label htmlFor="userguess">Guess a number from 1-{this.state.maxNum}:
-              <input type="text" id="userguess" name="userguess" value={this.state.currentInput} onChange={this.updateCurrentInput} /></label>
-              <button className="submitbtn" onClick={this.handleSubmit}>Submit</button>
-            </section>
+          <section className="inputcontainer">
+            <label htmlFor="userguess">Guess a number from 1-{this.state.maxNum}:
+            <input type="text" id="userguess" name="userguess" value={this.state.currentInput} onChange={this.updateCurrentInput} /></label>
+            <button className="submitbtn" onClick={this.handleSubmit}>Submit</button>
+          </section>
         </div>
         <div className="UserStats">
           <UserStats 
             guessCount={this.state.guessCount}
+            highScore={this.state.highScore}
             gameType={this.state.gameType}
             resetGame={this.resetGame}
           />
--- guessinggame/src/InitState.js
+++ guessinggame/src/InitState.js
@@ -2,15 +2,12 @@ import React from 'react';
 
 // what data is needed in game
 const initState = {
+	gameType: "",
   currentInput: '',
-  gameType: "",
   maxNum: 0,
   targetNumber: 0,
   guessCount: 0,
-  currentMessage: '',
-  gamesPlayed: 0,
-  standardHigh: 0,
-  expertHigh: 0,
+  highScore: 0,
 };
 
 export default initState;
